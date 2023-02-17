90ccd5943d681859c62d0a658f3ea13d8efa735f
Chars <1446713317@qq.com>
2023-02-17 11:42:45 +0800

    提交代码

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -34,8 +34,8 @@ const Home = memo(() => {
                 {
                     title: "基础部分",
                     list: [
-                        { title: "创建地图", url: "./codes/basic/create-map.js", image: "basic/create-map.png" },
-                        { title: "地图控件", url: "./codes/basic/map-controls.js", image: "basic/map-controls.png" }
+                        { title: "创建地图", url: "./codes/basic/create-map.js", image: "basic/create-map.jpg" },
+                        { title: "地图控件", url: "./codes/basic/map-controls.js", image: "basic/map-controls.jpg" }
                     ]
                 }
             ]
@@ -48,23 +48,23 @@ const Home = memo(() => {
                 {
                     title: "在线服务(互联网)",
                     list: [
-                        { title: "OSM白色底图", url: "./codes/layers/osm.js", image: "basic/create-map.png" },
-                        { title: "ArcGis蓝色底图", url: "./codes/layers/arcgis-blue.js", image: "layers/arcgis-blue.png" },
-                        { title: "ArcGis灰色底图", url: "./codes/layers/arcgis-gray.js", image: "layers/arcgis-gray.png" },
-                        { title: "ArcGis白色底图", url: "./codes/layers/arcgis-warm.js", image: "layers/arcgis-warm.png" },
-                        { title: "天地图蓝色底图", url: "./codes/layers/tianditu-blue.js", image: "layers/tianditu-blue.png" },
-                        { title: "天地图白色底图", url: "./codes/layers/tianditu-warm.js", image: "layers/tianditu-warm.png" },
-                        { title: "超图白色底图(深圳)", url: "./codes/layers/supermap-warm.js", image: "layers/supermap-warm.png" },
-                        { title: "超图蓝色底图(深圳)", url: "./codes/layers/supermap-blue.js", image: "layers/supermap-blue.png" },
+                        { title: "OSM白色底图", url: "./codes/layers/osm.js", image: "basic/create-map.jpg" },
+                        { title: "ArcGis蓝色底图", url: "./codes/layers/arcgis-blue.js", image: "layers/arcgis-blue.jpg" },
+                        { title: "ArcGis灰色底图", url: "./codes/layers/arcgis-gray.js", image: "layers/arcgis-gray.jpg" },
+                        { title: "ArcGis白色底图", url: "./codes/layers/arcgis-warm.js", image: "layers/arcgis-warm.jpg" },
+                        { title: "天地图蓝色底图", url: "./codes/layers/tianditu-blue.js", image: "layers/tianditu-blue.jpg" },
+                        { title: "天地图白色底图", url: "./codes/layers/tianditu-warm.js", image: "layers/tianditu-warm.jpg" },
+                        { title: "超图白色底图(深圳)", url: "./codes/layers/supermap-warm.js", image: "layers/supermap-warm.jpg" },
+                        { title: "超图蓝色底图(深圳)", url: "./codes/layers/supermap-blue.js", image: "layers/supermap-blue.jpg" },
                     ]
                 },
                 {
                     title: "信创云服务(内网)",
                     list: [
-                        { title: "天地图蓝色底图", url: "./codes/layers/tianditu-blue-1.js", image: "layers/tianditu-blue.png" },
-                        { title: "天地图白色底图", url: "./codes/layers/tianditu-warm-1.js", image: "layers/tianditu-warm.png" },
-                        { title: "规资蓝色底图", url: "./codes/layers/tgis-blue-4490.js", image: "layers/tgis-blue-4490.png" },
-                        { title: "规资白色底图", url: "./codes/layers/tgis-warm-4490.js", image: "layers/tgis-warm-4490.png" },
+                        { title: "天地图蓝色底图", url: "./codes/layers/tianditu-blue-1.js", image: "layers/tianditu-blue.jpg" },
+                        { title: "天地图白色底图", url: "./codes/layers/tianditu-warm-1.js", image: "layers/tianditu-warm.jpg" },
+                        { title: "规资蓝色底图", url: "./codes/layers/tgis-blue-4490.js", image: "layers/tgis-blue-4490.jpg" },
+                        { title: "规资白色底图", url: "./codes/layers/tgis-warm-4490.js", image: "layers/tgis-warm-4490.jpg" },
                         { title: "TGIS MVT图层", url: "./codes/layers/supermap-mvt.js", image: "layers/supermap-mvt.jpg" }
                     ]
                 }
@@ -78,11 +78,11 @@ const Home = memo(() => {
                 {
                     title: "基础对象",
                     list: [
-                        { title: "文字", url: "./codes/vector/entity-label.js", image: "vector/entity-label.png" },
-                        { title: "图片", url: "./codes/vector/entity-billboard.js", image: "vector/entity-billboard.png" },
-                        { title: "点", url: "./codes/vector/entity-point.js", image: "vector/entity-point.png" },
+                        { title: "文字", url: "./codes/vector/entity-label.js", image: "vector/entity-label.jpg" },
+                        { title: "图片", url: "./codes/vector/entity-billboard.js", image: "vector/entity-billboard.jpg" },
+                        { title: "点", url: "./codes/vector/entity-point.js", image: "vector/entity-point.jpg" },
                         { title: "线", url: "./codes/vector/entity-polyline.js", image: "vector/entity-polyline.jpg" },
-                        { title: "混合矢量", url: "./codes/vector/entity-mixin.js", image: "vector/entity-mixin.png" },
+                        { title: "混合矢量", url: "./codes/vector/entity-mixin.js", image: "vector/entity-mixin.jpg" },
                     ]
                 }
             ]
@@ -98,12 +98,13 @@ const Home = memo(() => {
                         { title: "测量工具", url: "./codes/scene-tools/measure.js", image: "scene-tools/measure.gif" },
                         { title: "绘制工具", url: "./codes/scene-tools/draw.js", image: "scene-tools/draw.gif" },
                         { title: "轨迹回放", url: "./codes/scene-tools/track-line.js", image: "scene-tools/track-line.gif" },
+                        { title: "聚合", url: "./codes/scene-tools/cluster.js", image: "scene-tools/cluster.gif" },
                     ]
                 },
                 {
                     title: "弹出层",
                     list: [
-                        { title: "弹窗", url: "./codes/scene-tools/popup.js", image: "scene-tools/popup.png" }
+                        { title: "弹窗", url: "./codes/scene-tools/popup.js", image: "scene-tools/popup.jpg" }
                     ]
                 }
             ]
@@ -116,7 +117,7 @@ const Home = memo(() => {
                 {
                     title: "Mapv",
                     list: [
-                        { title: "蜂巢图", url: "./codes/mapv/honeycomb.js", image: "mapv/honeycomb.png" },
+                        { title: "蜂巢图", url: "./codes/mapv/honeycomb.js", image: "mapv/honeycomb.jpg" },
                         { title: "热力图", url: "./codes/mapv/heatmap.js", image: "mapv/heatmap.gif" },
                         { title: "强边界图", url: "./codes/mapv/force-edge-bundling.js", image: "mapv/force-edge-bundling.gif" },
                         { title: "迁徙时序图", url: "./codes/mapv/migrate-time.js", image: "mapv/migrate-time.gif" },
