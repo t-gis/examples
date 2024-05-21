95d9961a148f0fe8f7d6c697888837e82934ad43
“LiElapse” <2052993504@qq.com>
2024-05-21 18:00:35 +0800

    添加turf

--- src/components/Stage/index.tsx
+++ src/components/Stage/index.tsx
@@ -24,6 +24,7 @@ const Sandcastle: ForwardRefRenderFunction<SandcastleHandle, SandcastleProps> = 
                     <link rel="stylesheet" href="./openlayers/v7.1.0/ol.css" />
                     <script src="./openlayers/v7.1.0/ol.js"></script>
                     <script src="./openlayers/v7.1.0/sfmap.js"></script>
+                    <script src="./turf.min.js"></script>
 
                     <script src="./map2d.iife.js"></script>
                     <style>
